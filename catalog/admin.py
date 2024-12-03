3ec1b7ea71b7b06b5e717da23add20c68b299ac6
Iyed <iyedabdelli73@gmail.com>
2024-12-03 17:31:26 +0100

    Customize admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["last_name", "first_name", "date_of_birth", "date_of_death"]
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
+    search_fields = ["first_name", "last_name"]
+    inlines = [BookInline]
+    list_per_page = 10
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "display_genre"]
+    search_fields = ["title"]
+    inlines = [BookInstanceInline]
+    list_per_page = 10
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ["id", "book", "status"]
+    list_filter = ["status", "due_back"]
+    list_per_page = 10
+
+    fieldsets = (
+        (None, {
+            "fields": ("book", "imprint", "id")
+        }),
+        ("Availability", {
+            "fields": ("status", "due_back")
+        }),
+    )
+
+
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
